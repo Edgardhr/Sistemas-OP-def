b824de338339ec5035c11b61a87db1624a03d9e9
Edgardhr <edgard.hernandez@estudiant.enti.cat>
2025-09-23 18:42:07 +0200

    Dia 3.2

---
+++ ProyectoEnClase/ProyectoEnClase/2InputSystem/InputSystem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "InputConst.h"
+#include <list>
+#include <map>
+#include <mutex>
+#include <functional>
+
+class InputSystem
+{
+
+};
+
--- ProyectoEnClase/ProyectoEnClase/ProyectoEnClase.cpp
+++ ProyectoEnClase/ProyectoEnClase/ProyectoEnClase.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-
+#include <string>
 #include "utils/ConsoleControler.h"
+#include <functional>
+#include <list>
 
+typedef std::function<int(int, int)> SumaFunction;
+typedef std::function<std::list<std::list<int>>> Test;
+void TestLambdasMolonas(std::function<void()> funcionMolona)
+{
+	std::cout << "Voy a ejecutar una funcion que estaba en una variable" << std::endl;
+
+	funcionMolona();
+
+	std::cout << "Ya la he ejecutado" << std::endl;
+}
 int main()
 {
-	CC::SetColor(CC::WHITE, CC::CYAN);
-	//ThreadTutorialTest();
+	std::string name = "Pepe";
+	std::list<std::list<int>> listaDeListas;
+	std::function<char()> funcionQueHaceCosasPeroEsUnaVariable = [name](){
+		
+		std::cout << "Esto no es como pero funciona y devuelve el nombre --> " << name <<std::endl;
+		return 54;
+		};
+	TestLambdasMolonas(funcionQueHaceCosasPeroEsUnaVariable);
 }
 
